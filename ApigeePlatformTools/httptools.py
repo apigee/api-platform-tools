2d24cb24f92bfafd599532ff5ef3f3bbee12e6c2
stonem12 <none@none>
2014-08-20 16:47:53 +0100

    httptools.py uses proxy settings from environment if set.

--- ApigeePlatformTools/httptools.py
+++ ApigeePlatformTools/httptools.py
@@ -1,8 +1,10 @@
-import base64;
-import httplib;
-import urlparse;
+import base64
+import httplib
+import urlparse
+import os
 
 opts = {}
+
 
 def setup(u, username, password):
   url = urlparse.urlparse(u) 
@@ -10,17 +12,42 @@ def setup(u, username, password):
   opts['httpHost'] = url[1]
   opts['userPW'] = base64.b64encode('%s:%s' % (username, password))
 
+
 def httpCall(verb, uri, headers = None, body = None):
-  if opts['httpScheme'] == 'https':
-    conn = httplib.HTTPSConnection(opts['httpHost'])
-  else:
-    conn = httplib.HTTPConnection(opts['httpHost'])
+  conn = _connect()
   if headers == None:
     hdrs = dict()
   else:
     hdrs = headers
   hdrs['Authorization'] = 'Basic %s' % opts['userPW']
   hdrs['Accept'] = 'application/json'
-  conn.request(verb, uri, body, hdrs)
+  url = "%(httpScheme)s://%(httpHost)s%(path)s" % dict(opts, path=uri)
+  conn.request(verb, url, body, hdrs)
   return conn.getresponse()
-  
+
+
+def _connect():
+  proxy = _getProxy(opts['httpScheme'])
+  host = (opts['httpHost'],)
+  if proxy:
+    real_host = host
+    host = proxy
+  if opts['httpScheme'] == 'https':
+    conn = httplib.HTTPSConnection(*host)
+  else:
+    conn = httplib.HTTPConnection(*host)
+  if proxy:
+    conn.set_tunnel(*real_host)
+  return conn
+
+
+def _getProxy(scheme):
+  """Reads proxy from environment"""
+  try:
+    proxy = os.environ[scheme + "_proxy"]
+    parts = proxy.split(":")
+    port = int(parts[-1]) if parts[-1].isdigit() else None
+    host = (parts[-2] if port else parts[-1]).strip("/")
+    return host, port
+  except KeyError:
+    return ()
